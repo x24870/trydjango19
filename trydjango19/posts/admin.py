91122b6a237e33ab234735e9ac50427a5ae7bb93
pyk <x24870@gmail.com>
2019-10-02 01:12:21 +0800

    add customized Admin model

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
+class PoseModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'updated', 'timestamp']
+    list_filter = ['updated', 'timestamp']
+    class Meta:
+        model = Post
 
 # Register your models here.
+admin.site.register(Post, PoseModelAdmin)
