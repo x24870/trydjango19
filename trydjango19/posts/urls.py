fc17349d9670206b322455d47ac5da970a22469e
pyk <x24870@gmail.com>
2019-10-02 16:04:44 +0800

    Link urls and views

---
+++ trydjango19/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
+    path('detail/', views.post_detail, name='post_detail'),
+    path('update/', views.post_update, name='post_update'),
+    path('delete/', views.post_delete, name='post_delete'),
+]
--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse 
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return HttpResponse("<h1>Retrun from post_list</h1>")
+
+def post_create(request):
+    return HttpResponse("<h1>Retrun from post_create</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>Retrun from post_detail</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Retrun from post_upadte</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Retrun from post_delete</h1>")
+
