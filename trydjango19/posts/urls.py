d8e2f0fd00b2b098628f0980b1239217acd9cc49
pyk <x24870@gmail.com>
2019-10-02 23:44:26 +0800

    Get absolute URL

--- trydjango19/posts/urls.py
+++ trydjango19/posts/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('create/', views.post_create, name='post_create'),
-    path('<pk>/', views.post_detail, name='post_detail'),
-    path('update/', views.post_update, name='post_update'),
-    path('delete/', views.post_delete, name='post_delete'),
+    path('', views.post_list, name='list'),
+    path('create/', views.post_create, name='create'),
+    path('<id>/', views.post_detail, name='detail'),
+    path('update/', views.post_update, name='update'),
+    path('delete/', views.post_delete, name='delete'),
 ]
