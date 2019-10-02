185d9f29d4d66ab2ef1bca8401638b678903e373
pyk <x24870@gmail.com>
2019-10-02 19:38:33 +0800

    dynamic url routing

--- trydjango19/posts/urls.py
+++ trydjango19/posts/urls.py
@@ -6,7 +6,7 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('create/', views.post_create, name='post_create'),
-    path('detail/', views.post_detail, name='post_detail'),
+    path('<pk>/', views.post_detail, name='post_detail'),
     path('update/', views.post_update, name='post_update'),
     path('delete/', views.post_delete, name='post_delete'),
 ]
--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -10,8 +10,8 @@ def post_list(request):
 def post_create(request):
     return HttpResponse("<h1>Retrun from post_create</h1>")
 
-def post_detail(request):
-    post = get_object_or_404(Post, id=2)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, id=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
 
 def post_update(request):
