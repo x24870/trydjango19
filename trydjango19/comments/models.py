58f051777d88beda771afe142267c42f1e3eaab4
pyk <x24870@gmail.com>
2020-01-08 23:52:03 +0800

    Django Generic Foriegn Keys Part1 (Use generic foriegnkey to replace foriegn key)

--- trydjango19/comments/models.py
+++ trydjango19/comments/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from django.conf import settings
@@ -8,7 +10,12 @@ from posts.models import Post
 # Create your models here.
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=SET_NULL, null=True)
-    post = models.ForeignKey(Post, on_delete=CASCADE)
+
+    #post = models.ForeignKey(Post, on_delete=CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
