ee13311297bf8541eede2cff42346b7dad866c6c
pyk <x24870@gmail.com>
2020-02-16 13:48:14 +0800

    Update & Delete in the Python Shell

--- trydjango19/posts/api/serializers.py
+++ trydjango19/posts/api/serializers.py
@@ -25,6 +25,9 @@ class PostDetailSerializer(ModelSerializer):
 
 
 """
+from posts.models import Post
+from posts.api.serializers import PostDetailSerializer
+
 data  = {
     'title': 'Yeah buddy',
     'content': 'New content',
@@ -32,7 +35,8 @@ data  = {
     'slug': 'yeah-buddu',
 }
 
-new_item = PostSerializer(data=data)
+obj = Post.objects.get(id=3)
+new_item = PostSerializer(obj, data=data)
 if new_item.is_valid():
     new_item.save()
 else:
