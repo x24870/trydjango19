bffb53b558e71bf8a9f472676cfc0d818ae13506
pyk <x24870@gmail.com>
2020-03-08 13:34:46 +0800

    Hyperlinked Identity Field for URL

--- trydjango19/posts/api/serializers.py
+++ trydjango19/posts/api/serializers.py
@@ -1,21 +1,34 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
 
 from posts.models import Post
 
+post_detail_url = HyperlinkedIdentityField(
+    view_name='posts-api:detail',
+    lookup_field='slug',
+)
+
 class PostListSerializer(ModelSerializer):
+    url = post_detail_url
+    # delete_url = HyperlinkedIdentityField(
+    #     view_name='posts-api:delete',
+    #     lookup_field='slug',
+    # )
     class Meta:
         model = Post
         fields = [
+            'url',
             'title',
-            'slug',
             'content',
             'publish',
+            # 'delete_url',
         ]
 
 class PostDetailSerializer(ModelSerializer):
+    url = post_detail_url
     class Meta:
         model = Post
         fields = [
+            'url',
             'id',
             'title',
             'slug',
