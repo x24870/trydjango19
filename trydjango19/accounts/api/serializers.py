a2588bb4899574917ceb41942ef4b4d736b78877
pyk <x24870@gmail.com>
2020-03-17 21:22:03 +0800

    ModelSerializer Create Method

--- trydjango19/accounts/api/serializers.py
+++ trydjango19/accounts/api/serializers.py
@@ -19,3 +19,12 @@ class UserCreateSerializer(ModelSerializer):
             'email',
         ]
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        email = validated_data['email']
+        user_obj = User(username=username, email=email)
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
