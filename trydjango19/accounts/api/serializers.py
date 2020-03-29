c3fbc227efab4195bbb54d308ffcae4c80e6221d
pyk <x24870@gmail.com>
2020-03-29 13:51:42 +0800

    UserLogin API Validation

--- trydjango19/accounts/api/serializers.py
+++ trydjango19/accounts/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
+from django.db.models import Q 
 
 from rest_framework.serializers import (
     CharField,
@@ -56,8 +57,8 @@ class UserCreateSerializer(ModelSerializer):
 
 class UserLoginSerializer(ModelSerializer):
     token = CharField(allow_blank=True, read_only=True)
-    username = CharField()
-    email = EmailField(label='Email address')
+    username = CharField(required=False, allow_blank=True)
+    email = EmailField(label='Email address', required=False, allow_blank=True)
     class Meta:
         model = User
         fields = [
@@ -69,10 +70,33 @@ class UserLoginSerializer(ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
+        user_obj = None
+        email = data.get('email', None)
+        username = data.get('username', None)
+        password = data['password'] # Because we didn't set password Field to allow_blank=True, so this field must has data
+        if not email and not username:
+            raise ValidationError('A username or email is required to login')
         # email = data['email']
         # user_qs = User.objects.filter(email=email)
         # if user_qs.exists():
         #     raise ValidationError('This email has already been registered')
+
+        user = User.objects.filter(
+            Q(email=email) |
+            Q(username=username)
+        ).distinct()
+        user = user.exclude(email__isnull=True).exclude(email__exact='')
+        if user.exists() and user.count()==1:
+            user_obj = user.first()
+        else:
+            raise ValidationError('This username/email is not valid')
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError('Incorrect credentials please try again.')
+
+        data['token'] = 'SOME RANDOM TOKEN'
+
         return data
 
     def validate_email2(self, value):
